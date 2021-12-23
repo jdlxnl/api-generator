05aab2335fc2b9862378454bd780afe026d872d1
Jeroen de Lau <jeroen.delau@gmail.com>
2021-12-23 10:27:23 +0100

    ✨ version 1

---
+++ src/JdlxApiGeneratorServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jdlx\ApiGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class JdlxApiGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+
+}
